33a234d31a9a2ff4f632a2d91507ea85db17f11e
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2021-02-03 22:39:29 -0300

    feat(exercise_1): make exercise number 1

---
+++ exercise_1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// EXERCISE 1. Using the short declaration operator, ASSING these VALUES to VARIABLES with the IDENTIFIERS "x", "y" and "z"
+	// and print the value stored in those variables using
+	// a. a single print statement
+	// b. multiple print statements
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
