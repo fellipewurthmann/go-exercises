a42e9b1d4ae0b51f1691736a9cbd69e1188bd43a
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2021-02-04 22:04:51 -0300

    feat(struct+exercise): make exercise-2 and fix project struct

---
+++ README.md
@@ -0,0 +1,3 @@
+## Go Exercises
+
+### The purpose of this repository is to do exercises and tests on `Go lang` every day and commit them here.
---
+++ exercises/exercise-2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	//  USE var to DECLARE three variables. The variables should have package level scope.
+	// Do not assign VALUES to the variables. Use the following IDENTIFIERS for the VARIABLES
+	// and make sure the variables are of the following TYPE(meaning they can store VALUES of that TYPE)
+	// a. identifier "x" type int
+	// b. identifier "y" type string
+	// c. identifier "z" type bool
+	// in func main print out the values for each identifier
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
