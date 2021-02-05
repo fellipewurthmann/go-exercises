24beb8f371544d2f82b72dedba37c43dada22362
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2021-02-04 22:34:33 -0300

    feat(exercise): make exercise-4 solution

---
+++ exercises/exercise-4/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type mytype int
+
+var x mytype
+
+func main() {
+	// For this exercise
+	// Create your own type. Have the underlying type be an int.
+	// create a VARIABLE of your new TYPE with the IDENTIFIER “x” using the “VAR” keyword
+	// in func main
+	// print out the value of the variable “x”
+	// print out the type of the variable “x”
+	// assign 42 to the VARIABLE “x” using the “=” OPERATOR
+	// print out the value of the variable “x”
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+}
