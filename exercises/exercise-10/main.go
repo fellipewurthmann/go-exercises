3ecf090efbea18b25a2ddd7fdea61c43d5b5500b
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2021-02-14 20:59:17 -0300

    exercise ten

---
+++ exercises/exercise-10/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Create a variable of type string using raw string literal. Print it.
+	rawString := `This is a raw string with literal value! "Quotes inside string!"`
+	fmt.Println(rawString)
+}
