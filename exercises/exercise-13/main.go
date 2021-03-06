39982273addf38ed073f48ba648c87b4ae4f0e2c
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2021-03-06 18:11:00 -0300

    13 exercise

---
+++ exercises/exercise-13/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: add exercise description
+	for i := 65; i <= 90; i++ {
+		fmt.Println(i)
+		for j := 0; j < 3; j++ {
+			fmt.Printf("\t%#U\n", i)
+		}
+	}
+}
